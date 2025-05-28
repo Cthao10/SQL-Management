42355b8c1ea2173092e0f191478ff0c3d3e21dd7
Cthao10 <152930382+Cthao10@users.noreply.github.com>
2025-05-28 18:12:26 -0500

    Delete error.php

--- error.php
+++
@@ -1,8 +0,0 @@
-<?php include('header.php'); ?>
-
-<div class="container mt-5">
-    <h2>Unexpected Error</h2>
-    <p>Sorry, something went wrong. Please try again later.</p>
-</div>
-
-<?php include('footer.php'); ?>
