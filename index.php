321f1c8599eb6a86817a04c6fc126816563b12d3
Cthao10 <152930382+Cthao10@users.noreply.github.com>
2025-05-28 18:19:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include 'view/header.php'; ?>
+<main class="container mt-4">
+    <nav>
+        <h2>Administrators</h2>
+        <ul>
+            <li><a href="manage_products.php">Manage Products</a></li>
+            <li><a href="manage_technicians.php">Manage Technicians</a></li>
+            <li><a href="manage_customers.php">Manage Customers</a></li>
+            <li><a href="under_construction.php">Create Incident</a></li>
+            <li><a href="under_construction.php">Assign Incident</a></li>
+            <li><a href="under_construction.php">Display Incidents</a></li>
+        </ul>
+
+        <h2>Technicians</h2>
+        <ul>
+            <li><a href="under_construction.php">Update Incident</a></li>
+        </ul>
+
+        <h2>Customers</h2>
+        <ul>
+            <li><a href="under_construction.php">Register Product</a></li>
+        </ul>
+    </nav>
+</main>
+<?php include 'view/footer.php'; ?>
